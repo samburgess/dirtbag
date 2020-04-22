480e4caa12c51bc01a52fe1f14b703ec8cd90db1
samburgess <saburgess239@gmail.com>
2020-04-21 23:26:17 -0700

    Updates

---
+++ precache-manifest.a84df69a18e0d58af1ca9301fec7fd7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf979e63d9d55d28111479f0b3eefffa",
+    "url": "/dirtbag/index.html"
+  },
+  {
+    "revision": "8a5387246820c3fd43d9",
+    "url": "/dirtbag/static/css/main.cbd6b5bc.chunk.css"
+  },
+  {
+    "revision": "414eee8a2210e285d938",
+    "url": "/dirtbag/static/js/2.4aa926e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dirtbag/static/js/2.4aa926e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5387246820c3fd43d9",
+    "url": "/dirtbag/static/js/main.1e1c27a1.chunk.js"
+  },
+  {
+    "revision": "1974d5446fe6723c388b",
+    "url": "/dirtbag/static/js/runtime-main.4184b9a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dirtbag/static/media/logo.5d5d9eef.svg"
+  }
+]);
